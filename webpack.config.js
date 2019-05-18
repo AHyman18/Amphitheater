3d48c1f6e9d96d34f3973d98702c8379a5b06ad5
letnewKid <tevinmcquilkin@gmail.com>
2019-05-17 20:45:28 -0400

    TM- file structure made

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  mode: 'development',
+  entry: path.join(__dirname, 'client', 'index.js'),
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['@babel/preset-env', '@babel/preset-react'],
+        },
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        loaders: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx'],
+  },
+  output: {
+    path: path.join(__dirname, 'build'),
+    filename: 'build.js',
+  },
+  plugins: [new webpack.HotModuleReplacementPlugin()],
+  devServer: {
+    contentBase: path.join(__dirname, '/public'),
+    port: 8080,
+    proxy: {
+      '/': 'http://localhost:3000',
+    },
+    publicPath: 'http://localhost:8080/build/',
+    hot: true,
+    historyApiFallback: true,
+  },
+};
