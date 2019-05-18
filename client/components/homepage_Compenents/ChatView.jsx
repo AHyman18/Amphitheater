17b28a53999d1a5d524c4160e0e4d919fa998334
letnewKid <tevinmcquilkin@gmail.com>
2019-05-18 13:42:07 -0400

    TM- created chatciew component

---
+++ client/components/homepage_Compenents/ChatView.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const chatWindow = styled.section;
+const MessageView = () => {
+  return (
+    <chatWindow>
+      <div>I am the chatWindow</div>
+    </chatWindow>
+  );
+};
