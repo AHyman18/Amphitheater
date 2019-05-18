9d381276ecd487171e546affed1b1d5763fbb8e2
Rodolfoguzman25 <rodolfoguzman147@gmail.com>
2019-05-18 06:22:21 -0400

    RG- created homepage, links to login and signin components

---
+++ client/components/App.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Login from './login'
+import Signin from './signin'
+
+function Home() {
+  return (
+    <div>
+      <h2>Welcome to Amphitheater.</h2>
+      <p>The app is designed to get fitness instructors and their trainees together in one space.</p>
+    </div>
+  );
+}
+
+function AppRouter() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/login/">Login</Link>
+            </li>
+            <li>
+              <Link to="/signin/">Signin</Link>
+            </li>
+          </ul>
+        </nav>
+        <Route path="/" exact component={Home} />
+        <Route path="/login/" component={Login} />
+        <Route path="/signin/" component={Signin} />
+      </div>
+    </Router>
+  );
+}
+
+export default AppRouter;
