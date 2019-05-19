4510921a3921878af6db9940750dae298c544885
letnewKid <tevinmcquilkin@gmail.com>
2019-05-19 09:24:47 -0400

    TM - created grid layout for home page

--- client/components/Homepage.jsx
+++ client/components/Homepage.jsx
@@ -4,14 +4,34 @@ import Navbar from './homepage_Compenents/Navbar';
 import ChatWindow from './homepage_Compenents/ChatWindow';
 
 const Homepage = styled.div`
-  display: flex;
-  justify-content: start;
-  flex-direction: column;
-`;
+  display: grid;
+  height: 100%;
+  grid-template-columns: repeat(6, 1fr);
+  grid-template-rows: 1fr 50px 50px 50px 50px;
+  grid-template-areas:
+    'nav nav nav nav nav nav'
+    'chat liv liv liv liv liv '
+    'chat liv liv liv liv liv '
+    'chat liv liv liv liv liv '
+    'chat liv liv liv liv liv ';
 
-export default () => (
+  grid-gap: 3px;
+`;
+// grid-template-rows: [row1-start] 25px [row1-end row2-start] 25px [row2-end];
+//   grid-template-columns: auto 50px auto;
+//   grid-templat-areas: 'nav nav nav nav' 'chat . . .' 'chat . . .';
+// // grid-template:
+// [row1-start] 'nav nav nav nav' 1fr [row1-end]
+// [row2-start] 'chat . . .' 200px [row2-end]
+// [row3-start] 'chat . . .' 200px [row3-end]
+// / auto 50px auto;
+
+const HomePage = () => (
   <Homepage>
     <Navbar />
+
     <ChatWindow />
+    {/* this is were the live stream compenet is going to be */}
   </Homepage>
 );
+export default HomePage;
--- client/components/homepage_Compenents/ChatWindow.jsx
+++ client/components/homepage_Compenents/ChatWindow.jsx
@@ -2,19 +2,25 @@ import React from 'react';
 import styled from 'styled-components';
 import Message from './Messages';
 
-export default () => {
-  const ChatWindow = styled.section`
+const ChatWindow = () => {
+  const ChatWindowStyled = styled.section`
     background-color: red;
     border: 2px solid black;
-    width: 30%;
-    height: 200px;
+    width: 100%;
     grid-area: chat;
   `;
+  // grid-column: 1/2;
+  // grid-row: 2/6;
+  //
+  // height: 200px;
+  // grid-area: chat;
   return (
-    <ChatWindow>
-      <div>I am the chatWindow</div>
+    <ChatWindowStyled>
+      <div>I am the ChatWindowStyled</div>
       <Message />
       {/* this is where the message input button goes */}
-    </ChatWindow>
+    </ChatWindowStyled>
   );
 };
+
+export default ChatWindow;
